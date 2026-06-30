294398fd802ab0240dee022409bf1aa21bc1885c
Will Jackson <wj@wovenandwoods.com>
2026-06-30 18:02:25 +0100

    Load header script on load

--- assets/js/header-scroll.js
+++ assets/js/header-scroll.js
@@ -1,14 +1,11 @@
-// Watches to see if the user scrolls down the page at least 33px.
-function headerScroll() {
-    const header = document.querySelector(".ww-header"); 
+(function () {
+    const header = document.querySelector(".ww-header");
     if (!header) {
         return
-    }; 
+    };
     function update() {
         header.classList.toggle("is-scrolled", window.scrollY > 41);
     }
-    window.addEventListener("scroll", update, { passive: true});
+    window.addEventListener("scroll", update, { passive: true });
     update();
-}
-
-headerScroll();
+})();
